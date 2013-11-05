6acdff5b4ebdcde0af80306a489790612fcb6d1b
rvasanda <rohit.vasandani@osxltrvasa.local>
2013-11-05 10:45:58 -0800

    initial commit

---
+++ src/main/java/CoucheDBMonitor.java
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: rohit.vasandani
+ * Date: 11/5/13
+ * Time: 10:43 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CoucheDBMonitor {
+
+
+}
---
+++ src/main/java/TestJson.java
@@ -0,0 +1,27 @@
+import com.google.gson.*;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+
+public class TestJson {
+	
+	public static void main(String[] args) {
+		System.out.println("Done");
+        try {
+            FileReader fileReader = new FileReader("conf/jsonString");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            StringBuilder jsonString = new StringBuilder();
+            String currentLine;
+            while ((currentLine = bufferedReader.readLine()) != null) {
+                jsonString.append(currentLine);
+            }
+            JsonObject jsonObject = new JsonParser().parse(jsonString.toString()).getAsJsonObject();
+            System.out.println("done");
+
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+	}
+}
