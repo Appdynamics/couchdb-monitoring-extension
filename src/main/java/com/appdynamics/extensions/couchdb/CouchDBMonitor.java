8036f34de1c85378f62b9f7d61fdbeb5b5fe0782
VishakaSekar <vishaka.sekar@appdynamics.com>
2019-09-17 18:49:02 -0700

    node metrics

--- src/main/java/com/appdynamics/extensions/couchdb/CouchDBMonitor.java
+++ src/main/java/com/appdynamics/extensions/couchdb/CouchDBMonitor.java
@@ -1,5 +1,5 @@
 /** 
- * Copyright 2013 AppDynamics 
+ * Copyright 2019 AppDynamics
  * 
  * Licensed under the Apache License, Version 2.0 (the License);
  * you may not use this file except in compliance with the License.
@@ -15,162 +15,81 @@
  */
 package com.appdynamics.extensions.couchdb;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
+import com.appdynamics.extensions.ABaseMonitor;
+import com.appdynamics.extensions.TasksExecutionServiceProvider;
+import com.appdynamics.extensions.couchdb.config.Stats;
+import com.appdynamics.extensions.util.AssertUtils;
+import com.google.common.collect.Maps;
+import com.singularity.ee.agent.systemagent.api.exception.TaskExecutionException;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.PatternLayout;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
-import org.apache.log4j.Logger;
+public class CouchDBMonitor extends ABaseMonitor {
 
-import com.appdynamics.TaskInputArgs;
-import com.appdynamics.extensions.PathResolver;
-import com.appdynamics.extensions.couchdb.api.CouchDBMetric;
-import com.appdynamics.extensions.couchdb.api.MetricAggregationType;
-import com.appdynamics.extensions.couchdb.config.ConfigUtil;
-import com.appdynamics.extensions.couchdb.config.Configuration;
-import com.appdynamics.extensions.couchdb.config.Server;
-import com.appdynamics.extensions.http.SimpleHttpClient;
-import com.google.common.base.Strings;
-import com.google.common.collect.Maps;
-import com.singularity.ee.agent.systemagent.api.AManagedMonitor;
-import com.singularity.ee.agent.systemagent.api.MetricWriter;
-import com.singularity.ee.agent.systemagent.api.TaskExecutionContext;
-import com.singularity.ee.agent.systemagent.api.TaskOutput;
-import com.singularity.ee.agent.systemagent.api.exception.TaskExecutionException;
+    @Override
+    protected String getDefaultMetricPrefix() {
+        return Constants.DEFAULT_METRIC_PREFIX;
+    }
 
-public class CouchDBMonitor extends AManagedMonitor {
+    @Override
+    public String getMonitorName() {
+        return Constants.COUCHDB_MONITOR;
+    }
 
-	private static final Logger logger = Logger.getLogger("com.singularity.extensions.CouchDBMonitor");
-	private static final String METRICS_SEPARATOR = "|";
-	public static final String CONFIG_ARG = "config-file";
-	// To load the config files
-	private final static ConfigUtil<Configuration> configUtil = new ConfigUtil<Configuration>();
+    @Override
+    protected void initializeMoreStuff (Map<String, String> args) {
+        this.getContextConfiguration().setMetricXml(args.get("metrics-file"), Stats.class);
+    }
 
-	public CouchDBMonitor() {
-		String msg = "Using Monitor Version [" + getImplementationVersion() + "]";
-		logger.info(msg);
-		System.out.println(msg);
-	}
+    @Override
+    protected void doRun(TasksExecutionServiceProvider tasksExecutionServiceProvider) {
+        List<Map<String, ?>> servers =  getServers();
 
-	/**
-	 * Main execution method that uploads the metrics to the AppDynamics
-	 * Controller
-	 * 
-	 * @see com.singularity.ee.agent.systemagent.api.ITask#execute(java.util.Map,
-	 *      com.singularity.ee.agent.systemagent.api.TaskExecutionContext)
-	 */
-	public TaskOutput execute(Map<String, String> taskArguments, TaskExecutionContext taskExecutionContext) throws TaskExecutionException {
-		if (taskArguments != null) {
-			logger.info("Starting the CouchDB Monitoring Task");
-			if (logger.isDebugEnabled()) {
-				logger.debug("Task Arguments Passed ::" + taskArguments);
-			}
-			// initialize(taskArguments);
-			String configFilename = getConfigFilename(taskArguments.get(CONFIG_ARG));
-			try {
-				// read the config.
-				Configuration config = configUtil.readConfig(configFilename, Configuration.class);
-				if (config != null && config.getServers() != null) {
-					for (Server hostConfig : config.getServers()) {
-						Map<String, String> httpClientArguments = buildHttpClientArguments(hostConfig);
-						SimpleHttpClient httpClient = SimpleHttpClient.builder(httpClientArguments).build();
+        for (Map<String, ?> server : servers) {
+            CouchDBMonitorTask task = new CouchDBMonitorTask(tasksExecutionServiceProvider.getMetricWriteHelper(), this.getContextConfiguration(),server);
+            AssertUtils.assertNotNull(server.get(Constants.DISPLAY_NAME), "The displayName can not be null");
+            tasksExecutionServiceProvider.submit(server.get(Constants.DISPLAY_NAME).toString(), task);
+        }
+    }
 
-						CouchDBWrapper couchDBWrapper = new CouchDBWrapper();
-						List<CouchDBMetric> metrics = couchDBWrapper.gatherMetrics(httpClient);
-						printMetrics(config, hostConfig, metrics);
-						logger.info("Gathered and Printed metrics successfully for " + hostConfig.getDisplayName());
-					}
-					logger.info("CouchDB Monitoring task completed successfully.");
-					return new TaskOutput("CouchDB Monitoring task completed successfully.");
-				}
-			} catch (FileNotFoundException e) {
-				logger.error("Config file not found :: " + configFilename, e);
-			} catch (Exception e) {
-				logger.error("Metrics collection failed", e);
-			}
-		}
-		logger.info("CouchDB monitoring task completed with failures.");
-		throw new TaskExecutionException("CouchDB monitoring task completed with failures.");
-	}
+    @Override
+    protected List<Map<String, ?>> getServers() {
+        return (List<Map<String, ?>>) getContextConfiguration().getConfigYml().get(Constants.SERVERS);
+    }
 
-	private Map<String, String> buildHttpClientArguments(Server hostConfig) {
-		Map<String, String> taskArguments = Maps.newHashMap();
-		taskArguments.put(TaskInputArgs.HOST, hostConfig.getHost());
-		taskArguments.put(TaskInputArgs.PORT, String.valueOf(hostConfig.getPort()));
-		taskArguments.put(TaskInputArgs.USER, hostConfig.getUsername());
-		taskArguments.put(TaskInputArgs.PASSWORD, hostConfig.getPassword());
+    public static void main(String[] args) throws TaskExecutionException, IOException {
 
-		return taskArguments;
-	}
+        ConsoleAppender ca = new ConsoleAppender();
+        ca.setWriter(new OutputStreamWriter(System.out));
+        ca.setLayout(new PatternLayout("%-5p [%t]: %m%n"));
+        ca.setThreshold(Level.DEBUG);
+        org.apache.log4j.Logger.getRootLogger().addAppender(ca);
 
-	/**
-	 * Writes the couchDB metrics to the controller
-	 * 
-	 * @param config
-	 *            Name of the CouchDB host
-	 * @param hostConfig
-	 * @param metrics
-	 *            HashMap containing all the couchDB metrics
-	 */
-	private void printMetrics(Configuration config, Server hostConfig, List<CouchDBMetric> metrics) throws Exception {
-		String metricPrefix = config.getMetricPrefix();
-		for (CouchDBMetric metric : metrics) {
-			String metricCategory = metric.getMetricCategory();
-			String metricName = metric.getMetricName();
-			Map<MetricAggregationType, BigDecimal> metricStats = metric.getValues();
-			for (Entry<MetricAggregationType, BigDecimal> typedValues : metricStats.entrySet()) {
-				String aggregationType = typedValues.getKey().getAggregationType();
-				BigDecimal metricValue = typedValues.getValue();
-				printMetric(metricPrefix + hostConfig.getDisplayName() + METRICS_SEPARATOR + metricCategory + METRICS_SEPARATOR + metricName
-						+ METRICS_SEPARATOR + aggregationType, metricValue);
-			}
-		}
-	}
 
-	/**
-	 * Returns the metric to the AppDynamics Controller.
-	 * 
-	 * @param metricName
-	 * @param metricValue
-	 * @throws Exception
-	 */
-	private void printMetric(String metricName, BigDecimal metricValue) throws Exception {
-		if (metricValue != null) {
-			MetricWriter metricWriter = super.getMetricWriter(metricName, MetricWriter.METRIC_AGGREGATION_TYPE_OBSERVATION,
-					MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE, MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_INDIVIDUAL);
-			String value = metricValue.setScale(0, RoundingMode.HALF_UP).toString();
-			metricWriter.printMetric(value);
-			logger.debug(metricName + " : " + value);
-		}
-	}
+        final CouchDBMonitor monitor = new CouchDBMonitor();
+        final Map<String, String> taskArgs = Maps.newHashMap();
+        taskArgs.put("config-file", "/Users/vishaka.sekar/AppDynamics/couchdb-monitoring-extension/src/main/resources/conf/config.yml");
+        taskArgs.put("metrics-file", "/Users/vishaka.sekar/AppDynamics/couchdb-monitoring-extension/src/main/resources/conf/metrics.xml");
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(new Runnable() {
+            public void run() {
+                try {
+                    monitor.execute(taskArgs, null);
+                } catch (Exception e) {
 
-	/**
-	 * Returns a config file name,
-	 * 
-	 * @param filename
-	 * @return String
-	 */
-	private String getConfigFilename(String filename) {
-		if (filename == null) {
-			return "";
-		}
-		// for absolute paths
-		if (new File(filename).exists()) {
-			return filename;
-		}
-		// for relative paths
-		File jarPath = PathResolver.resolveDirectory(AManagedMonitor.class);
-		String configFileName = "";
-		if (!Strings.isNullOrEmpty(filename)) {
-			configFileName = jarPath + File.separator + filename;
-		}
-		return configFileName;
-	}
+                    System.out.println("Error while running the task"+ e);
+                }
+            }
+        }, 2, 60, TimeUnit.SECONDS);
 
-	private static String getImplementationVersion() {
-		return CouchDBMonitor.class.getPackage().getImplementationTitle();
-	}
+    }
 }
